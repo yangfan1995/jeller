9c545155026ca0d370f924a5038c7bd83ae4684c
qizi <qizi@qizideMacBook-Pro.local>
2022-03-11 17:15:45 +0800

    init

---
+++ Shared/jellerApp.swift
@@ -0,0 +1,17 @@
+//
+//  jellerApp.swift
+//  Shared
+//
+//  Created by qizi on 2022/3/11.
+//
+
+import SwiftUI
+
+@main
+struct jellerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
